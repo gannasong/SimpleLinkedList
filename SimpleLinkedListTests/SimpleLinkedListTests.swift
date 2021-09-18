385a7eeef741c0319445fed7c357482cb12d4fe1
Miguel <gannasong@gmail.com>
2021-09-18 14:16:27 +0800

    init project

---
+++ SimpleLinkedListTests/SimpleLinkedListTests.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleLinkedListTests.swift
+//  SimpleLinkedListTests
+//
+//  Created by SUNG HAO LIN on 2021/9/18.
+//
+
+import XCTest
+
+class SimpleLinkedListTests: XCTestCase {
+
+  func test() {
+    
+  }
+
+}
