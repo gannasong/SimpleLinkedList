9921b60e2b1a67ddd28b8a2f8f611fd38caac63b
Miguel <gannasong@gmail.com>
2021-09-19 15:29:44 +0800

    improve test cases

--- SimpleLinkedListTests/SimpleLinkedListTests.swift
+++ SimpleLinkedListTests/SimpleLinkedListTests.swift
@@ -10,8 +10,8 @@ import SimpleLinkedList
 
 class SimpleLinkedListTests: XCTestCase {
 
-  func test_init_doesNotStoreAnyNode() {
-    let sut = makeSUT()
+  func test_init_linkedListIsEmpty() {
+    let sut = makeSUT(type: String.self)
 
     XCTAssertTrue(sut.isEmpty)
     XCTAssertEqual(sut.count, 0)
@@ -20,54 +20,40 @@ class SimpleLinkedListTests: XCTestCase {
   }
 
   func test_append_appendOneValueToLinkedList() {
-    let value = "any-node"
-    let sut = makeSUT()
+    let value = "1"
+    let sut = makeSUT(type: String.self)
 
-    sut.append(value: value)
-
-    XCTAssertEqual(sut.count, 1)
-    XCTAssertFalse(sut.isEmpty)
-    XCTAssertNotNil(sut.first)
-    XCTAssertNotNil(sut.last)
-    XCTAssertEqual(sut.first?.value, value)
-    XCTAssertEqual(sut.last?.value, value)
+    expectAppend(sut: sut, values: [value]) {
+      XCTAssertEqual(sut.first?.value, value)
+      XCTAssertEqual(sut.last?.value, value)
+    }
   }
 
   func test_append_appendTwoNodeToLinkedList() {
-    let value = "any-node"
-    let anotherValue = "another-node"
-    let sut = makeSUT()
+    let values = ["1", "2"]
+    let sut = makeSUT(type: String.self)
 
-    sut.append(value: value)
-    sut.append(value: anotherValue)
-
-    XCTAssertEqual(sut.count, 2)
-    XCTAssertEqual(sut.first?.value, value)
-    XCTAssertEqual(sut.last?.value, anotherValue)
+    expectAppend(sut: sut, values: values) {
+      XCTAssertEqual(sut.first?.value, values.first)
+      XCTAssertEqual(sut.last?.value, values.last)
+    }
   }
 
   func test_remove_removeAllNodes() {
-    let samples = ["1", "2", "3", "4", "5"]
-    let sut = makeSUT()
+    let values = ["1", "2", "3", "4", "5"]
+    let sut = makeSUT(type: String.self)
 
-    samples.forEach { value in
-      sut.append(value: value)
+    expectRemoveAll(sut: sut, values: values) {
+      XCTAssertEqual(sut.count, 0)
+      XCTAssertTrue(sut.isEmpty)
+      XCTAssertNil(sut.first)
+      XCTAssertNil(sut.last)
     }
-
-    XCTAssertEqual(sut.count, 5)
-    XCTAssertEqual(sut.first?.value, "1")
-    XCTAssertEqual(sut.last?.value, "5")
-
-    sut.removeAll()
-    XCTAssertEqual(sut.count, 0)
-    XCTAssertTrue(sut.isEmpty)
-    XCTAssertNil(sut.first)
-    XCTAssertNil(sut.last)
   }
 
   func test_remove_removeNodesFromLinkedList() {
     let samples = ["1", "2", "3", "4", "5"]
-    let sut = makeSUT()
+    let sut = makeSUT(type: String.self)
 
     samples.forEach { value in
       sut.append(value: value)
@@ -89,11 +75,11 @@ class SimpleLinkedListTests: XCTestCase {
   }
 
   func test_remove_removeFirstNodeFromLinkedList() {
-    let samples = ["1", "2", "3", "4", "5"]
-    let sut = makeSUT()
+    let values = ["1", "2", "3", "4", "5"]
+    let sut = makeSUT(type: String.self)
     var results: [String?] = []
 
-    samples.forEach { value in
+    values.forEach { value in
       sut.append(value: value)
     }
 
@@ -113,9 +99,37 @@ class SimpleLinkedListTests: XCTestCase {
 
   // MARK: - Helper
 
-  private func makeSUT(file: StaticString = #file, line: UInt = #line) -> SimpleLinkedList<String> {
-    let sut = SimpleLinkedList<String>()
+  private func makeSUT<T>(type: T.Type, file: StaticString = #file, line: UInt = #line) -> SimpleLinkedList<T> {
+    let sut = SimpleLinkedList<T>()
     trackForMemoryLeaks(sut, file: file, line: line)
     return sut
+  }
+
+  private func expectAppend<T>(sut: SimpleLinkedList<T>, values: [T], _ assert: () -> Void) {
+    let expectedCount = values.count
+
+    values.forEach { value in
+      sut.append(value: value)
+    }
+
+    XCTAssertEqual(sut.count, expectedCount)
+    XCTAssertFalse(sut.isEmpty)
+    XCTAssertNotNil(sut.first)
+    XCTAssertNotNil(sut.last)
+
+    assert()
+  }
+
+  private func expectRemoveAll<T>(sut: SimpleLinkedList<T>, values: [T], _ assert: () -> Void) {
+    values.forEach { value in
+      sut.append(value: value)
+    }
+
+    sut.removeAll()
+
+    XCTAssertEqual(sut.count, 0)
+    XCTAssertTrue(sut.isEmpty)
+    XCTAssertNil(sut.first)
+    XCTAssertNil(sut.last)
   }
 }
